1b2d9df4acd6541d045d95d2c4e57e883cb64812
wdesimini <wdesimini8@gmail.com>
2021-03-05 18:51:26 -0800

    get km fonts, implement in home view

---
+++ RecreatingDesignInSwiftUI/Kul_Mobile/Resource/KulMobile+Font.swift
@@ -0,0 +1,25 @@
+//
+//  KulMobile+Font.swift
+//  RecreatingDesignInSwiftUI
+//
+//  Created by Wilson Desimini on 3/5/21.
+//
+
+import SwiftUI
+
+// family Superclarendon
+//   font Superclarendon-Regular
+//   font Superclarendon-BoldItalic
+//   font Superclarendon-Light
+//   font Superclarendon-BlackItalic
+//   font Superclarendon-Italic
+//   font Superclarendon-LightItalic
+//   font Superclarendon-Bold
+//   font Superclarendon-Black
+
+private let family = "Superclarendon"
+
+extension Font {
+    static func kmRegular(size: CGFloat) -> Font { .custom("\(family)-Regular", size: size) }
+    static func kmBold(size: CGFloat) -> Font { .custom("\(family)-Bold", size: size) }
+}
--- RecreatingDesignInSwiftUI/Kul_Mobile/View/KulMobileHomeView.swift
+++ RecreatingDesignInSwiftUI/Kul_Mobile/View/KulMobileHomeView.swift
@@ -9,12 +9,19 @@ import SwiftUI
 
 struct KulMobileHomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Basic Teen Culture")
+                .font(.kmBold(size: 72))
+            
+            Spacer()
+        }
     }
 }
 
 struct KulMobileHomeView_Previews: PreviewProvider {
     static var previews: some View {
-        KulMobileHomeView()
+        NavigationView {
+            KulMobileHomeView()
+        }
     }
 }

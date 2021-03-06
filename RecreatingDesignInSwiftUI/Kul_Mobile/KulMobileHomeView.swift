c072cbf2d2a0aedbe23ecd5b04b2489ef267e69c
wdesimini <wdesimini8@gmail.com>
2021-03-05 18:37:22 -0800

    add Kul Mobile dir

---
+++ RecreatingDesignInSwiftUI/Kul_Mobile/KulMobileHomeView.swift
@@ -0,0 +1,20 @@
+//
+//  KulMobileHomeView.swift
+//  RecreatingDesignInSwiftUI
+//
+//  Created by Wilson Desimini on 3/5/21.
+//
+
+import SwiftUI
+
+struct KulMobileHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct KulMobileHomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        KulMobileHomeView()
+    }
+}

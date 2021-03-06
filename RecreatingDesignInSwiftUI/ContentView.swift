37207f248de43de5f008171835e33d75414d6526
wdesimini <wdesimini8@gmail.com>
2021-03-05 18:35:11 -0800

    Initial Commit

---
+++ RecreatingDesignInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecreatingDesignInSwiftUI
+//
+//  Created by Wilson Desimini on 3/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecreatingDesignInSwiftUI/RecreatingDesignInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecreatingDesignInSwiftUIApp.swift
+//  RecreatingDesignInSwiftUI
+//
+//  Created by Wilson Desimini on 3/5/21.
+//
+
+import SwiftUI
+
+@main
+struct RecreatingDesignInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

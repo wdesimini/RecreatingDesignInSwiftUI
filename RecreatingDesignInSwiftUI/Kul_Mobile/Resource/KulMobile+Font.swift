404e1bee461441a028a0744a0cc8e47dc1646067
wdesimini <wdesimini8@gmail.com>
2021-03-07 11:28:54 -0800

    refactor km font service

--- RecreatingDesignInSwiftUI/Kul_Mobile/Resource/KulMobile+Font.swift
+++ RecreatingDesignInSwiftUI/Kul_Mobile/Resource/KulMobile+Font.swift
@@ -7,20 +7,8 @@
 
 import SwiftUI
 
-// family Superclarendon
-//   font Superclarendon-Regular
-//   font Superclarendon-BoldItalic
-//   font Superclarendon-Light
-//   font Superclarendon-BlackItalic
-//   font Superclarendon-Italic
-//   font Superclarendon-LightItalic
-//   font Superclarendon-Bold
-//   font Superclarendon-Black
-
-private let family = "Superclarendon"
-
 extension Font {
-    static func kmRegular(size: CGFloat) -> Font { .custom("\(family)-Regular", size: size) }
-    static func kmBold(size: CGFloat) -> Font { .custom("\(family)-Bold", size: size) }
+    static func kmRegular(size: CGFloat) -> Font { .custom("Superclarendon-Regular", size: size) }
+    static func kmBold(size: CGFloat) -> Font { .custom("Superclarendon-Bold", size: size) }
     static func kmSecondaryRegular(size: CGFloat) -> Font { .custom("Staatliches-Regular", size: size) }
 }

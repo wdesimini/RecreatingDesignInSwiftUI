bbf7e63b4dca5d5ae3d72f40a9f87e53fea76d5f
wdesimini <wdesimini8@gmail.com>
2021-03-07 16:38:28 -0800

    add el font service

---
+++ RecreatingDesignInSwiftUI/Recreations/Energy_Life/Resource/EnergyLife+Font.swift
@@ -0,0 +1,13 @@
+//
+//  EnergyLife+Font.swift
+//  RecreatingDesignInSwiftUI
+//
+//  Created by Wilson Desimini on 3/7/21.
+//
+
+import SwiftUI
+
+extension Font {
+    static func elFont(size: CGFloat) -> Font { .custom("Avenir-Book", size: size) }
+    static func elMediumFont(size: CGFloat) -> Font { .custom("Avenir-Medium", size: size) }
+}

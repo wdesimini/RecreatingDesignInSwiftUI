878c6e0eea1504c025a633bdcb82146551ed954f
wdesimini <wdesimini8@gmail.com>
2021-03-07 16:23:31 -0800

    add el color service

---
+++ RecreatingDesignInSwiftUI/Recreations/Energy_Life/Resource/EnergyLife+Color.swift
@@ -0,0 +1,37 @@
+//
+//  EnergyLife+Color.swift
+//  RecreatingDesignInSwiftUI
+//
+//  Created by Wilson Desimini on 3/7/21.
+//
+
+import SwiftUI
+
+extension Color {
+    private enum ELColor {
+        case red, teal, purple, orange
+        
+        var rgb: (r: Double, g: Double, b: Double) {
+            switch self {
+            case .red:
+                return (252, 86, 104)
+            case .teal:
+                return (65, 213, 225)
+            case .purple:
+                return (77, 82, 224)
+            case .orange:
+                return (253, 142, 55)
+            }
+        }
+    }
+    
+    private init(elColor: ELColor) {
+        let rgb = elColor.rgb
+        self.init(r: rgb.r, g: rgb.g, b: rgb.b)
+    }
+    
+    static var elRed: Color { .init(elColor: .red) }
+    static var elTeal: Color { .init(elColor: .teal) }
+    static var elPurple: Color { .init(elColor: .purple) }
+    static var elOrange: Color { .init(elColor: .orange) }
+}

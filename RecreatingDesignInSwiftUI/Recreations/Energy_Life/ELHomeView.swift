59e88b265db824fb873768fcf675ce7be767514b
wdesimini <wdesimini8@gmail.com>
2021-03-07 16:09:47 -0800

    add new recreation energy life

---
+++ RecreatingDesignInSwiftUI/Recreations/Energy_Life/ELHomeView.swift
@@ -0,0 +1,22 @@
+//
+//  ELHomeView.swift
+//  RecreatingDesignInSwiftUI
+//
+//  Created by Wilson Desimini on 3/7/21.
+//
+
+import SwiftUI
+
+// reference https://dribbble.com/shots/7171328-Mobile-app-Energy-Life
+
+struct ELHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ELHomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        ELHomeView()
+    }
+}
